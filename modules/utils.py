d99c6bb53f3ca17541e638e6b6d16575a64feee2
Rei Hakurei <reimu_after_marisa@yahoo.com>
2024-03-29 11:34:39 +0700

    support callable async

--- modules/utils.py
+++ modules/utils.py
@@ -1,6 +1,13 @@
+from inspect import iscoroutinefunction as callableasync
 from modules import shared
 
 async def is_owner(ctx):
   return ctx.author.id in shared.config.users
+
+async def call_function(fun, *args, **kwargs):
+  if callableasync(fun):
+    await fun(*args, **kwargs)
+  elif callable(fun):
+    fun(*args, **kwargs)
 
 is_manager = is_owner
